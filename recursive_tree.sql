d8e202b1a72c622efaa5ce64a3c4392197f40139
Camden Cheek <camden@ccheek.com>
2022-12-02 22:24:15 -0700

    add another query

--- recursive_tree.sql
+++ recursive_tree.sql
@@ -12,16 +12,35 @@ WITH RECURSIVE paths (path, kind, oid) AS (
     JOIN tree_entries te
     ON te.tree_oid = paths.oid AND paths.kind == 3
 )
-SELECT path, hex(oid) 
+SELECT path, hex(paths.oid)
+FROM paths WHERE kind == 4;
+
+-- Given 1000 random blobs, find which ones are reachable from
+-- some commit
+WITH RECURSIVE paths (path, kind, oid) AS (
+    SELECT te.name, te.kind, te.oid
+    FROM tree_entries te
+    JOIN commits ON commits.tree_oid = te.tree_oid
+    WHERE commits.oid = X'd61f40b2f1aed793d13c393e66bf8d10fc8fff41'
+
+    UNION ALL
+
+    SELECT paths.path || '/' || te.name, te.kind, te.oid
+    FROM paths
+    JOIN tree_entries te
+    ON te.tree_oid = paths.oid AND paths.kind == 3
+)
+SELECT path, hex(paths.oid)
 FROM paths 
+JOIN (SELECT oid FROM blobs ORDER BY random() LIMIT 1000) bl
+    ON bl.oid = paths.oid
 WHERE kind == 4;
 
--- List commits and paths a blob can be found in
+-- List commits and paths a blob can be found in (slower than the previous query)
 WITH RECURSIVE trees (tree_oid, path) AS (
     SELECT tree_oid, name
     FROM tree_entries
-    -- WHERE oid = X'2124AAE8AA0815067498FE215156089FAF528760'
-    WHERE oid IN (SELECT oid FROM blobs ORDER BY random() LIMIT 1)
+    WHERE oid = X'0d43989d2e1987bc91a20021d942c95b844d9e07'
 
     UNION ALL
 
@@ -32,4 +51,5 @@ WITH RECURSIVE trees (tree_oid, path) AS (
 )
 SELECT hex(commits.oid), path
 FROM trees
-JOIN commits ON trees.tree_oid = commits.tree_oid;
+JOIN commits ON trees.tree_oid = commits.tree_oid
+WHERE commits.oid = X'd61f40b2f1aed793d13c393e66bf8d10fc8fff41';
